d1088349b81d2d4fa3ec413ffb84f0aabc17009f
gabriel-santossilva <37078877+gabriel-santossilva@users.noreply.github.com>
2018-04-03 18:36:59 -0300

    Delete exercicio5_pag43.py

--- exercicio5_pag43.py
+++
@@ -1,7 +0,0 @@
-def inverso(frase):
-    r = frase.split()
-    for i in range(len(r)):
-        r[i] = r[i][:: -1]
-    return ' '.join(r)
-frase = input('digite uma frase ')
-print(inverso(frase))
