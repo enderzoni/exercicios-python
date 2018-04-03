bb01397b7178a8a7ffd4fe27467d7c494ac0b1ec
gabriel-santossilva <37078877+gabriel-santossilva@users.noreply.github.com>
2018-04-03 18:35:59 -0300

    Delete exercicio1_pag43(2).py

--- exercicio1_pag43(2).py
+++
@@ -1,7 +0,0 @@
-def conversao(fahrenheit):
-    fahrenheit = int (fahrenheit)
-    celsius = (5/9) * fahrenheit - 32
-    int (fahrenheit)
-    return celsius
-fahrenheit = input('digite a temperatura em fahrenheit e converta pra celsius ')
-print (conversao(fahrenheit)) 
