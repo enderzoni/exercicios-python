01e63214c17bf9cb59a0b744c73670b6bf328b3c
gabriel-santossilva <37078877+gabriel-santossilva@users.noreply.github.com>
2018-04-03 18:36:49 -0300

    Delete exercicio4_pag43.py

--- exercicio4_pag43.py
+++
@@ -1,8 +0,0 @@
-def stat(dic):
-    s = sum(dic.values())
-    med = s / len(dic.values())
-    var = max(dic.values()) - min(dic.values())
-    return s, med, var
-d = { chr(v): v for v in range(55, 70)}
-print(d)
-print(stat(d))
